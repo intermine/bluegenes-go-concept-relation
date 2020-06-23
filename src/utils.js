cd18b195fd05dfc6e9b902effb16d66fb3a66480
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-24 03:04:34 +0530

    added pie node sharing colors of their parent nodes color

--- src/utils.js
+++ src/utils.js
@@ -58,12 +58,37 @@ function getGraphData(data) {
 	return elements;
 }
 
+function makePie(elements) {
+	const idToColorsMap = elements.reduce((m, elem) => {
+		if (elem.data && elem.data.id) {
+			if (!m[elem.data.id]) m[elem.data.id] = [];
+			m[elem.data.id].push(elem.data.bg);
+		}
+		return m;
+	}, {});
+	return Object.keys(idToColorsMap)
+		.filter(key => idToColorsMap[key].length > 1)
+		.map(id => {
+			const pieNodeStyle = {};
+			pieNodeStyle['selector'] = `node[id = '${id}']`;
+			const style = {};
+			const colorLen = idToColorsMap[id].length;
+			for (let i = 1; i <= colorLen; i++) {
+				style[`pie-${i}-background-color`] = idToColorsMap[id][i - 1];
+				style[`pie-${i}-background-size`] = 100 / colorLen;
+			}
+			pieNodeStyle['style'] = style;
+			return pieNodeStyle;
+		});
+}
+
 function createCytoscapeConfig(elements) {
 	return {
 		container: document.getElementById('cy'),
 		elements: elements,
 		grabbable: true,
 		style: [
+			...makePie(elements),
 			{
 				selector: 'node',
 				style: {
