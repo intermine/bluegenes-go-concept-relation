71a2fc010be1f6ad84fdf0a46184bbd2950432b1
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-26 00:35:37 +0530

    added toggle button style in filter panel

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -3,9 +3,20 @@ import React from 'react';
 const FilterPanel = ({ updateFilters, selectedOntology, ontologyList }) => {
 	return (
 		<div className="filter-panel-root">
-			<h4 className="filter-panel-title">Available Ontologies</h4>
+			<h4 className="filter-panel-title">Filter Panel</h4>
+			<hr />
 			<div className="filter-panel">
 				<div className="filter-container">
+					<div className="node-filter">
+						<div>All Nodes</div>
+						<div>
+							<label className="switch">
+								<input type="checkbox" />
+								<span className="slider round"></span>
+							</label>
+						</div>
+					</div>
+					<div className="ontology-filter">Available Ontologies</div>
 					{ontologyList.map(term => (
 						<React.Fragment key={term}>
 							<input
