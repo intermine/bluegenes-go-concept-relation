867656accaab9b3e8b666ace0fb6be08dbd0fa24
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-26 22:10:25 +0530

    separated out data containing multiple parents and stored in state object

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -9,6 +9,7 @@ const RootContainer = ({ serviceUrl, entity }) => {
 	const [ontologyList, setOntologyList] = useState([]);
 	const [selectedOntology, changeOntology] = useState('');
 	const [selectedOntologyData, setOntologyData] = useState({});
+	const [sharedNodeData, setSharedNodeData] = useState({});
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -39,8 +40,8 @@ const RootContainer = ({ serviceUrl, entity }) => {
 	}, [data]);
 
 	useEffect(() => {
-		// Formatting data according to the unique ontologies so that it can be passed directly to the graph 
-		// when filter is applied. Setting the data in advance will allow us to use it directly when needed  
+		// Formatting data according to the unique ontologies so that it can be passed directly to the graph
+		// when filter is applied. Setting the data in advance will allow us to use it directly when needed
 		// instead of processing it whenever filter is changed
 		const filteredMap = ontologyList.reduce(
 			(curMap, ontology) => ({
@@ -58,6 +59,55 @@ const RootContainer = ({ serviceUrl, entity }) => {
 		// set default selected ontology to the first ontology mentioned in the list
 		changeOntology(ontologyList.length && ontologyList[0]);
 	}, [ontologyList]);
+
+	useEffect(() => {
+		const sharedNodeMap = {};
+		Object.keys(selectedOntologyData).map(ontology => {
+			const data = selectedOntologyData[ontology];
+			// created a map to count the nodes with multiple parents
+			// key: node symbol, Value: [freq, [parents array]]
+			const counts = new Map();
+			data.forEach(geneData => {
+				geneData &&
+					geneData.goAnnotation &&
+					geneData.goAnnotation.filter(item => {
+						let count = counts.get(item.ontologyTerm.identifier);
+						if (typeof count !== 'undefined') {
+							var freq = count[0];
+							var gene = count[1];
+						}
+						counts.set(
+							item.ontologyTerm.identifier,
+							count ? [freq + 1, [...gene, geneData]] : [1, [geneData]]
+						);
+						return count === 1;
+					});
+			});
+			const map = {};
+			// Traversing map and if count is greater then 1 i.e. shared node
+			// then filtering it out and storing according to ontology namespace
+			for (const [key, value] of counts.entries()) {
+				const freq = value[0],
+					geneArr = value[1];
+				if (freq > 1) {
+					const filteredMap = geneArr.map(item => ({
+						...item,
+						goAnnotation: item.goAnnotation.filter(
+							g =>
+								g.ontologyTerm.identifier === key &&
+								g.ontologyTerm.namespace === ontology
+						)
+					}));
+					filteredMap.forEach(item => {
+						if (!map[item.symbol]) map[item.symbol] = item;
+						else map[item.symbol].goAnnotation.push(...item.goAnnotation);
+					});
+				}
+			}
+			sharedNodeMap[ontology] = [...Object.values(map)];
+		});
+		setSharedNodeData(sharedNodeMap);
+	}, [selectedOntologyData]);
 
 	return (
 		<div className="rootContainer">
